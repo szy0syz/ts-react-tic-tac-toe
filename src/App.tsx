1d7978ab388dee279433dd90c6f9fccd74d3f9e8
jerry <szy0syz@gmail.com>
2019-08-24 01:08:10 +0800

    Update 渲染棋盘和旗子

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
-import Plate from './components/Plate';
+// import Form from './components/Form';
+// import Plate from './components/Plate';
 
 const App: React.FC = () => {
+  const [ board, setBoard ] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const renderCell = (index: number) => (
+    <div className='cell'></div>
+  )
+
+  const renderBoard = () => (
+    <div className='board-container'>
+      {board.map((_, index) => renderCell(index))}
+    </div>
+  )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Form name={'jerry'}></Form>
-        <Plate num={123456}></Plate>
+        {renderBoard()}
+        {/* <Form name={'jerry'}></Form>
+        <Plate num={123456}></Plate> */}
       </header>
     </div>
   );
