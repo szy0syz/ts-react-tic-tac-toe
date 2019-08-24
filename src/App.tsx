6619ef8cf8e19bfc4c0fda05610a47845a5995e4
jerry <szy0syz@gmail.com>
2019-08-24 09:42:40 +0800

    Update 落棋子变色功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Form from './components/Form';
-// import Plate from './components/Plate';
 
 const App: React.FC = () => {
   const [ board, setBoard ] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [ nextPlayer, setNextPlayer ] = useState(1);
+
+  const handleCellClick = (index: number) => {
+    if (board[index] !== 0) return;
+
+    const newBoard = board.slice();
+    newBoard[index] = nextPlayer;
+    setBoard(newBoard);
+    setNextPlayer(3 - nextPlayer);
+  }
 
   const renderCell = (index: number) => (
-    <div className='cell'></div>
+    <div className='cell' key={index} data-player={board[index]} onClick={() => handleCellClick(index)}></div>
   )
 
   const renderBoard = () => (
@@ -17,13 +25,22 @@ const App: React.FC = () => {
     </div>
   )
 
+  const renderStatus = () => {
+    return (
+      <div style={{marginTop: '30px'}}>
+        { 'Player 1 is red.' } <br />
+        { 'Player 2 is blue.' } <br />
+        { 'Game is ongoing.' }
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {renderBoard()}
-        {/* <Form name={'jerry'}></Form>
-        <Plate num={123456}></Plate> */}
+        {renderStatus()}
       </header>
     </div>
   );
